b26a0469ea6241984579860888294dde6957aaec
Rena Goldsmith <rena.kg@gmail.com>
2016-06-26 22:55:43 +1200

    wrap elements in div tags

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.redirect('/kaenga')
+});
+
+router.get('/kaenga', function(req, res, next) {
+    res.render('index', { title: 'Act-Mai Draw-Mai' });
+});
+
+router.get('/ngature', function(req, res, next) {
+    res.render('ngature', { title: 'Nga Ture'});
 });
 
 module.exports = router;
