8d747c329312b37bd700a9d12c13d182b2686f9a
Rena Goldsmith <rena.kg@gmail.com>
2016-07-04 23:16:26 +1200

    style index hbs

--- routes/teams.js
+++ routes/teams.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
     res.send('you should not see this')
 });
 
+router.get('/new', function(req, res, next) {
+    res.send('make your team here')
+})
+
 module.exports = router;
